5c1b6c9f647f135c00820839e93c900630da9890
theMladyPan <stanislav@rubint.sk>
2018-09-01 10:27:37 +0200

    Images loaded only after modal opened

--- coodise/static/js/main.js
+++ coodise/static/js/main.js
@@ -6,11 +6,23 @@ $(document).ready(function(){
     }
   );
 
+  $('.modal.fade').on('show.bs.modal', function(){
+    // if modal is shown
+    var preview_div = $(this).find('.picture_preview');
+    // <img class="rounded preview" src=""/>
+    var img = $("<img />").attr('src', preview_div.text());
+    img.attr('class', 'rounded preview');
+    preview_div.text("");
+    preview_div.append(img);
+
+      }
+  );
+
   $('.badge.spoiler').click(
     // when clicked on badge inside link
     function(event){
-       $(this).parent().prop("title","Doubleclick to open link");
-       $(this).parent().click(
+      $(this).parent().prop("title","Doubleclick to open link");
+      $(this).parent().click(
         // overide links click functionality and do not redirect
         function(event){
           event.preventDefault();
